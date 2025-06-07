d7af591868662d32c7550890d65ea9d6aa946273
richa <richamariajohnson@gmail.com>
2025-06-07 12:48:07 +0530

    page layout

---
+++ ambulance_tracker/lib/registration/basic.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class homepage extends StatefulWidget {
+  const homepage({super.key});
+
+  @override
+  State<homepage> createState() => _homepageState();
+}
+
+class _homepageState extends State<homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        title: Align(
+          alignment: Alignment.topLeft,
+          child: Image.asset(
+            'assets/title.png',
+            fit: BoxFit.contain,
+            height: 180,
+          ),
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color.fromRGBO(159, 13, 55, 1.0),
+              Color.fromRGBO(189, 83, 114, 1.0),
+            ],
+          ),
+        ),
+        padding: EdgeInsets.only(
+          top: MediaQuery.of(context).padding.top + kToolbarHeight,
+          left: 10.0,
+          right: 10.0,
+          bottom: 10.0,
+        ),
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+}
