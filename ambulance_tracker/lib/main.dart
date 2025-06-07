fd30beb1c765a79d10fefb76cc1b4b3fc476f37b
richa <richamariajohnson@gmail.com>
2025-06-07 14:46:38 +0530

    fonts

--- ambulance_tracker/lib/main.dart
+++ ambulance_tracker/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:ambulance_tracker/registration/basic.dart';
+import 'package:ambulance_tracker/registration/driver.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       debugShowCheckedModeBanner: false,
-      home: homepage(),
+      home: DriverRegistration(),
     );
   }
 }
--- ambulance_tracker/lib/registration/driver.dart
+++ ambulance_tracker/lib/registration/driver.dart
@@ -1,1 +1,66 @@
 import 'package:flutter/material.dart';
+
+class DriverRegistration extends StatefulWidget {
+  const DriverRegistration({super.key});
+
+  @override
+  State<DriverRegistration> createState() => DriverRegistrationState();
+}
+
+class DriverRegistrationState extends State<DriverRegistration> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        title: Align(
+          alignment: Alignment.topLeft,
+          child: Image.asset(
+            'assets/title.png',
+            fit: BoxFit.contain,
+            height: 180,
+          ),
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color.fromRGBO(159, 13, 55, 1.0),
+              Color.fromRGBO(189, 83, 114, 1.0),
+            ],
+          ),
+        ),
+        padding: EdgeInsets.only(
+          top: MediaQuery.of(context).padding.top + kToolbarHeight,
+          left: 10.0,
+          right: 10.0,
+          bottom: 10.0,
+        ),
+        child: Container(
+          width: double.infinity,
+          height: double.infinity,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: Colors.white,
+          ),
+          child: Container(
+            alignment: Alignment.topCenter,
+            padding: EdgeInsets.only(top: 20.0),
+            child: SelectionContainer.disabled(
+              child: Text(
+                "DRIVER REGISTRATION",
+                style: TextStyle(fontSize: 32, fontFamily: 'Roboto'),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
