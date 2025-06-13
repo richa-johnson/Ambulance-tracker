b774aeaa3a0736fb9f5d4621977a761a2d5d2d45
devatheertha <devatheerthavakayad@gmail.com>
2025-06-13 10:30:26 +0530

    api,controller

---
+++ backend/app/Http/Controllers/AuthManager.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthManager extends Controller
+{
+    function userregister(Request $request){
+
+    }
+
+    function driverlogin(Request $request){
+
+    }
+
+    function login  (Request $request){
+
+    }
+}
