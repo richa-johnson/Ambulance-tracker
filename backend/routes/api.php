d47e1bc020962171a8352d31e9cb1b54207b59f7
Diya Betcy <diyabetcy123@gmail.com>
2025-06-23 21:55:20 +0530

    backend

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthManager;
 use App\Http\Controllers\BookingController;
+use App\Http\Controllers\DriverProfileController;
 use App\Http\Controllers\ProfileController;
 use App\Models\Booking;
 use Illuminate\Http\Request;
@@ -25,6 +26,20 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/auth/user',[AuthManager::class,'user']);
     Route::put('/driver/status',[DriverStatus::class,'updateDriverStatus']);
 });
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/booking/store',[BookingController::class,'store']);
+    Route::get('/booking/{booking}/response',[BookingController::class,'respond']);
+    Route::get('/user/confirmed-booking', [BookingController::class, 'getConfirmedBooking']);
+    Route::get('/driver/confirmed-booking', [BookingController::class, 'getDriverConfirmedBooking']);
+    Route::post('/booking/{bookingId}/patients', [BookingController::class, 'storePatients']);
+    Route::get('/booking/check-expiry', [BookingController::class, 'expireIfStillPending']);
+    Route::get('/booking/{booking}/status', [BookingController::class, 'getBookingStatus']);
+    Route::post('/booking/{id}/confirm', [BookingController::class, 'confirm']);
+    Route::post('/booking/{id}/cancel', [BookingController::class, 'cancel']);
+    Route::post('/booking/{id}/complete', [BookingController::class, 'complete']);
+});
+
+
 
 Route::middleware(['auth:sanctum'])->get('/admin/users',[AdminController::class,'getAllUsers']);
 Route::get('/user/availableAmbulances',[DriverController::class,'getAvailabledrivers']);
@@ -34,18 +49,11 @@ Route::middleware('auth:sanctum')
       ->post('/driver/location', [DriverController::class, 'updateLocation']);
 Route::get('/admin/trackAmbulance/driver/{query}',[DriverController::class, 'getDriverLocation']);
 
-Route::middleware(['auth:sanctum'])->post('/booking/store',[BookingController::class,'store']);
-Route::middleware(['auth:sanctum'])->get('/booking/{booking}/response',[BookingController::class,'respond']);
+Route::middleware(['auth:sanctum'])->post('/bookings/{booking}/patients', [BookingController::class, 'storePatients']);
 Route::middleware('auth:sanctum')->get('/user/UserDetails', [DriverController::class, 'getSignedInUserDetails']);
-Route::middleware(['auth:sanctum'])->get('/booking/check-expiry', [BookingController::class, 'expireIfStillPending']);
-Route::middleware(['auth:sanctum'])->get('/booking/{booking}/status', [BookingController::class, 'getBookingStatus']);
 
 Route::middleware('auth:sanctum')->get('/driver/pending-bookings', [BookingController::class, 'pending']);
 
-Route::middleware('auth:sanctum')->post('/booking/{id}/confirm', [BookingController::class, 'confirm']);
-Route::middleware('auth:sanctum')->post('/booking/{id}/cancel', [BookingController::class, 'cancel']);
-Route::middleware('auth:sanctum')->post('/booking/{id}/complete', [BookingController::class, 'complete']);
-Route::post('/booking/{id}/patients', [BookingController::class, 'storePatients']);
 
 Route::middleware('auth:sanctum')->get('/booking/driver-history', [BookingController::class, 'getdriverHistory']);
 
@@ -56,3 +64,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::put('/profile',  [ProfileController::class, 'update']); // PUT/PATCH update
 });
 Route::middleware('auth:sanctum')->get('/driver/status', [DriverController::class, 'status']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get ('/driver/profile',  [DriverProfileController::class, 'show']);
+    Route::put ('/driver/profile',  [DriverProfileController::class, 'update']);
+});
